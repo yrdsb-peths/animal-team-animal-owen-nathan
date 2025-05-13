186e41e05442e9b37f9754240bfcdd2f81e9f6e9
owenchlee <owenlees888@gmail.com>
2025-05-13 14:32:15 -0400

    Edited boost class descriptions

--- Boost.java
+++ Boost.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Boost here.
+ * When boost is picked up, elephant speed increases permanently
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Lee
+ * @version May 13
  */
 public class Boost extends Actor
 {
